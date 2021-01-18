6aa5a0dbc50db54fe83dd03730aadfbcf2fb4846
sajith <sajith@digitalxlabs.com>
2021-01-18 23:43:25 +0530

    Added 4d version of KF

---
+++ laser_measurements/tracking.h
@@ -0,0 +1,36 @@
+//
+// Created by sajith on 1/18/21.
+//
+
+#ifndef KALMAN_FILTER_TRACKING_H
+#define KALMAN_FILTER_TRACKING_H
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include "kalman_filter.h"
+#include "measurement_package.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+
+class Tracking
+{
+public:
+    Tracking();
+
+    virtual ~Tracking();
+
+    void ProcessMeasurement(const MeasurementPackage &measurement_pack);
+
+    KalmanFilter kf_;
+
+private:
+    bool is_initialized_;
+    int64_t previous_timestamp_;
+    float noise_ax;
+    float noise_ay;
+};
+
+#endif //KALMAN_FILTER_TRACKING_H

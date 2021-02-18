7840b6f5c90ddc6db1a9d66475bb4458d47e1ae4
sajith <sajithgithub@gmail.com>
2021-02-18 19:20:20 +0530

    Improved

--- naive_bayes/classifier.h
+++ naive_bayes/classifier.h
@@ -5,4 +5,29 @@
 #ifndef KALMAN_FILTER_CLASSIFIER_H
 #define KALMAN_FILTER_CLASSIFIER_H
 
+#include <string>
+#include <vector>
+#include "../Eigen/Dense"
+
+using Eigen::ArrayXd;
+using std::string;
+using std::vector;
+
+class GNB
+{
+public:
+
+    // constructor
+    GNB();
+
+    // destructor
+    virtual ~GNB();
+
+    void train(const vector<vector<double>> &data, const vector<string> &labels);
+
+    string predict(const vector<double> &sample);
+
+    vector<string> possible_labels{"left", "keep", "right"};
+};
+
 #endif //KALMAN_FILTER_CLASSIFIER_H

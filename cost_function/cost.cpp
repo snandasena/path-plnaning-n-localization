7e082032cfbbece340b06521d5d13a07cf3933e4
sajith <sajithgithub@gmail.com>
2021-02-19 15:46:15 +0530

    Implented cost function

--- cost_function/cost.cpp
+++ cost_function/cost.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "cost.h"
+#include <cmath>
+
+double goal_distance_cost(int goal_lane, int intended_lane, int final_lane,
+                          double distance_to_goal)
+{
+    double delta_d = 2.0 * goal_lane - intended_lane - final_lane;
+    return 1 - exp(-(std::abs(delta_d / distance_to_goal)));
+}
--- cost_function/cost.h
+++ cost_function/cost.h
@@ -5,6 +5,8 @@
 #ifndef KALMAN_FILTER_COST_H
 #define KALMAN_FILTER_COST_H
 
+double goal_distance_cost(int goal_lane, int intended_lane, int final_lane,
+                          double distance_to_goal);
 
 
 #endif //KALMAN_FILTER_COST_H

f524142a36b098c5236b6f0828733e3db4389089
sajith <sajithgithub@gmail.com>
2021-02-19 17:05:20 +0530

    Improved

--- cost_function/cost.cpp
+++ cost_function/cost.cpp
@@ -11,3 +11,10 @@ double goal_distance_cost(int goal_lane, int intended_lane, int final_lane,
     double delta_d = 2.0 * goal_lane - intended_lane - final_lane;
     return 1 - exp(-(std::abs(delta_d / distance_to_goal)));
 }
+
+
+double inefficiency_cost(int target_speed, int intended_lane, int final_lane,
+                         const std::vector<int> &lane_speeds)
+{
+
+}
--- cost_function/cost.h
+++ cost_function/cost.h
@@ -5,8 +5,14 @@
 #ifndef KALMAN_FILTER_COST_H
 #define KALMAN_FILTER_COST_H
 
+#include <vector>
+
 double goal_distance_cost(int goal_lane, int intended_lane, int final_lane,
                           double distance_to_goal);
+
+
+double inefficiency_cost(int target_speed, int intended_lane, int final_lane,
+                         const std::vector<int> &lane_speeds);
 
 
 #endif //KALMAN_FILTER_COST_H
